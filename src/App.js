728ce5267ee8629e16bc600dfef840a9233941d7
Jason Terhorst <yourstruly@jterhorst.com>
2019-08-18 20:01:49 -0500

    Testing tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import TestStyles from './containers/TestStyles/TestStyles';
 
 class App extends Component {
   
@@ -16,6 +16,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/test" exact component={TestStyles} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>          
         </Layout>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducer';
+
+import './styles/tailwind.css';
 
 const store = createStore(reducer);
 
