567160910a79d3b013ea8f06d415804087f4a1a0
Jason Terhorst <yourstruly@jterhorst.com>
2019-08-25 07:24:17 -0500

    Testing

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,4 +23,9 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+
+    it('should render logout item if authed', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
